f68af020509e0fa2588a7894273f1e150116c70b
EliHeuman <biz.elih@gmail.com>
2021-03-22 15:07:47 +0200

    Zoom map to 13.5

--- mapanimation.js
+++ mapanimation.js
@@ -5,7 +5,7 @@ var markers = [];
 // load map
 function init(){
 	var myOptions = {
-		zoom      : 13,
+		zoom      : 13.5,
 		center    : { lat:42.353350,lng:-71.091525},
 		mapTypeId : google.maps.MapTypeId.ROADMAP
 	};
