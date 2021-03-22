be166025a69068e2f04d751a4952535b9adbeb15
EliHeuman <biz.elih@gmail.com>
2021-03-22 14:11:45 +0200

    First Commit

---
+++ mapanimation.js
@@ -0,0 +1,86 @@
+
+var map;
+var markers = [];
+
+// load map
+function init(){
+	var myOptions = {
+		zoom      : 14,
+		center    : { lat:42.353350,lng:-71.091525},
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+	var element = document.getElementById('map');
+  	map = new google.maps.Map(element, myOptions);
+  	addMarkers();
+}
+
+// Add bus markers to map
+async function addMarkers(){
+	// get bus data
+	var locations = await getBusLocations();
+  console.log(new Date);
+	// loop through data, add bus markers
+	locations.forEach(function(bus){
+		var marker = getMarker(bus.id);		
+		if (marker){
+			moveMarker(marker,bus);
+		}
+		else{
+			addMarker(bus);			
+		}
+	});
+
+	// timer
+	setTimeout(addMarkers,15000);
+}
+
+// Request bus data from MBTA
+async function getBusLocations(){
+	var url = 'https://api-v3.mbta.com/vehicles?sort=direction_id&include=route&filter%5Broute%5D=1&api_key=5480903a65764e129cf91c942b08de49';	
+	var response = await fetch(url);
+	var json = await response.json();
+	return json.data;
+}
+
+function addMarker(bus){
+	var icon = getIcon(bus);
+	var marker = new google.maps.Marker({
+	    position: {
+	    	lat: bus.attributes.latitude, 
+	    	lng: bus.attributes.longitude
+	    },
+	    map: map,
+	    icon: icon,
+	    id: bus.id
+	});
+	markers.push(marker);
+}
+
+function getIcon(bus){
+	// select icon based on bus direction
+	if (bus.attributes.direction_id === 0) {
+		return './Images/Red.png';
+	}
+	return './Images/Blue.png';	
+}
+
+function moveMarker(marker,bus) {
+	// change icon if bus has changed direction
+	var icon = getIcon(bus);
+	marker.setIcon(icon);
+
+	// move icon to new lat/lon
+    marker.setPosition( {
+    	lat: bus.attributes.latitude, 
+    	lng: bus.attributes.longitude
+	});
+}
+
+function getMarker(id){
+	var marker = markers.find(function(item){
+		return item.id === id;
+	});
+	return marker;
+}
+
+window.onload = init;
